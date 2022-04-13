1dcbbc542d13f40228dfa0306df04a56833a4026
Manmohit Matharu <manmohit.matharu@gmail.com>
2022-04-13 16:22:35 -0400

    updating play.js

--- play.js
+++ play.js
@@ -13,11 +13,36 @@ const connect = function () {
     console.log(data.toString())
   })
 
+  conn.on('connect', () => {
+    console.log('connected')
+    conn.write('Name: Zig')
+    setInterval(() => {conn.write('Move: up')}, 500)
+    setTimeout(() => {conn.write('Move: up')}, 500)
+    setTimeout(() => {conn.write('Move: up')}, 500)
+    conn.write('Move: left')
+    conn.write("Move: left")
+    conn.write('Move: down')
+    setTimeout(() => {conn.write('Move: right')}, 500)
+  })
+
   return conn;
 };
 
 console.log("connecting...");
 connect();
+
+
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on("data", (handleUserInput) => {
+
+  });
+  return stdin;
+};
+
 
 module.exports = {
   connect
